8ac3342e8ee9a598c8e61f71adb644b7bf60f5a5
DK\psk <psk@sitecore.net>
2017-06-19 17:15:45 -0400

    Code clean up, reflection usage fix

--- src/Sitecore.Support.167606/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.167606/Properties/AssemblyInfo.cs
@@ -4,3 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Sitecore.Support.167606")]
 [assembly: AssemblyProduct("Sitecore.Support.167606")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Use Search instead of Filter for Azure Search queries")]
+
